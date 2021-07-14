6ac4778227cde83a225e90afc2c437237efed7b4
Merk42 <mark_c@markecurtis.com>
2021-07-14 17:41:56 -0400

    nav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Output, EventEmitter, Input  } from '@angular/core';
 export class SidenavComponent implements OnInit {
   @Output() chosencontent = new EventEmitter<any>();
   @Input() content: any;
+  // activeIndex: number;
   constructor() { }
   
   ngOnInit(): void {
