1cc2bfa6f74b21aa4677a7f309882c6aa197d44d
Merk42 <mark_c@markecurtis.com>
2021-07-16 17:18:18 -0400

    top nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RoutesRecognized } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-site';
-
+  mainNav = [
+    {
+      "id":"about",
+      "link":"/profile",
+      "name":"Profile"
+    },
+    {
+      "id":"past",
+      "link":"/websites",
+      "name":"Websites"
+    },
+    {
+      "id":"cg",
+      "link":"/cg",
+      "name":"3D"
+    }
+  ]
+  currentIndex = 0;
+  constructor(router:Router) {
+    router.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        this.currentIndex = this.mainNav.findIndex(x => x.link === event.url);
+        console.log('currentIndex is', this.currentIndex);
+      }
+      // NavigationEnd
+      // NavigationCancel
+      // NavigationError
+      // RoutesRecognized
+    });
+  }
+  
   toggleMenu(){
     console.log('toggle')
     const BODYDOM = document.getElementsByTagName('body')[0] as HTMLElement;
