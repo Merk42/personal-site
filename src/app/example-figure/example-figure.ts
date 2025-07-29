8fb6ccffbc061a237da4621135c08d15c7377ba6
Merk42 <mark_c@markecurtis.com>
2025-07-29 10:23:47 -0400

    ngOptimizedImage

--- src/app/example-figure/example-figure.ts
+++ src/app/example-figure/example-figure.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common'
 import { Component, input } from '@angular/core';
 import { Example } from '../types';
 @Component({
   selector: 'app-example-figure',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './example-figure.html',
-  styleUrl: './example-figure.css'
+  styleUrl: './example-figure.css',
 })
 export class ExampleFigure {
   readonly EX = input.required<Example>()
