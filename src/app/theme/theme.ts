79676a00885bda9e15c6e7e6ec5e633feb530918
Merk42 <mark_c@markecurtis.com>
2025-11-18 11:24:03 -0500

    new way to change theme

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -1,6 +1,6 @@
-import { Component, effect, model} from '@angular/core';
+import { Component, effect, inject, model} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'mec-theme',
   imports: [FormsModule],
@@ -9,11 +9,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class Theme {
   hue = model(150);
-
+  meta = inject(Meta);
   constructor() {
     effect(() => {
       const DEG = this.hue() +'deg';
       document.documentElement.style.setProperty('--hue', DEG);
+      const BASE = .05;
+      const C = 0;
+      const pi = 3.14;
+      const CALC = BASE + (Math.sin(0.6 * pi) * C);
+      const THEMECOLOR = `oklch(50% ${CALC} ${DEG});`
+      this.meta.updateTag({ content: THEMECOLOR }, 'name=theme-color');
     })
   }
 
