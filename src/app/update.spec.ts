86e92a3282d19998873ce048b13ffca198375709
Merk42 <mark_c@markecurtis.com>
2026-03-19 08:59:30 -0400

    fix test

--- src/app/update.spec.ts
+++ src/app/update.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { SwUpdate } from '@angular/service-worker';
 import { Update } from './update';
 
 describe('Update', () => {
   let service: Update;
 
+
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+         { provide: SwUpdate, useValue: { isEnabled: false } }
+      ]
+    })
     TestBed.configureTestingModule({});
     service = TestBed.inject(Update);
   });
